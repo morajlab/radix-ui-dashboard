c27fff8c8c258330c4ee97dfcd12b6c50a485599
Morteza Jamali <mortezajamali4241@gmail.com>
2022-07-25 17:04:57 +0000

    chore(packages): create `types` package

---
+++ packages/components/components/src/lib/button/button.stories.tsx
@@ -0,0 +1,13 @@
+import { Button } from '.';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {};
---
+++ packages/types/src/index.ts
@@ -0,0 +1,5 @@
+import type { IBareProps } from '@morajlab/npm.react.components.bare';
+import type { MLComponent } from '@morajlab/npm.react.types.common';
+
+export interface IRadixUIDashProps extends IBareProps {}
+export type RadixUIDashComponent<T = {}> = MLComponent<T>;
