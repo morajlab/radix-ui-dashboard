c49c2339550108eca7aa4000fd4d74bcd6545a3d
Morteza Jamali <mortezajamali4241@gmail.com>
2022-07-27 03:35:10 +0000

    chore(base): format some files

--- packages/components/components-e2e/src/integration/components-components/components-components.spec.ts
+++ packages/components/components-e2e/src/integration/components-components/components-components.spec.ts
@@ -1,7 +1,7 @@
 describe('components-components: ComponentsComponents component', () => {
   beforeEach(() => cy.visit('/iframe.html?id=componentscomponents--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to ComponentsComponents!');
-    });
+
+  it('should render the component', () => {
+    cy.get('h1').should('contain', 'Welcome to ComponentsComponents!');
+  });
 });
